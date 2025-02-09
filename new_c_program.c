46b68ef88a73cf447c986b703061deb3cc8d471a
paulboul1013 <paulboul1013@gmail.com>
2025-02-09 09:26:35 +0800

    更新114研究所考試心得

--- new_c_program.c
+++ new_c_program.c
@@ -3,32 +3,14 @@
 
 
 
+
 int main(){
 
+   
+ 
     
-    int total=0;
-    int id;
-    do{
-        scanf("%d",&id);
-        switch (id)
-        {
-            case 1:
-            total+=90;
-            break;
-            case 2:
-            total+=75;
-            break;
-            case 3:
-            total+=89;
-            break;
 
-       
-        }
-    }while(id!=0);
 
-    printf("total: %d\n",total);
-
-    
 
     return 0;
 
