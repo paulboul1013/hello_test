acda9948909b70e9549384f3854782752683771c
paulboul1013 <paulboul1013@gmail.com>
2025-02-28 23:20:20 +0800

    c語言getline和copy實作

--- main.c
+++ main.c
@@ -143,10 +143,55 @@ int isspace(int c){
     
 }
 
+#define MAXLINE 1000
+int getline_2(char line[],int maxline);
+void copy_2(char to[],char from[]);
+
+
+
 int main(){
 
+    int len;
+    int max;
+    char  line[MAXLINE];
+    char longest[MAXLINE];
 
-    
+    max=0;
+    while((len=getline_2(line,MAXLINE))>0){
+        if (len>max){
+            max=len;
+            copy_2(longest,line);
+        }
+    }
+
+    if (max>0){
+        printf("%s",longest);
+    }
 
     return 0;
+}
+
+
+int getline_2(char s[],int lim){
+    int c,i;
+    for(i=0;i<lim-1&&(c=getchar())!=EOF&&c!='\n';i++){
+        s[i]=c;
+        if (c=='\n'){
+            s[i]=c;
+            i++;
+        }
+    }
+
+
+    s[i]='\0';
+    return i;
+}
+
+void copy_2(char to[],char from[]){
+    int i;
+    i=0;
+    while((to[i]=from[i])!='\0'){
+        ++i;
+    }
+    
 }
