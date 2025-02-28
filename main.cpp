ee190ab39989efe44522811b8d214b89db1e9bef
paulboul1013 <paulboul1013@gmail.com>
2025-02-28 23:20:57 +0800

    LCS實作

--- main.cpp
+++ main.cpp
@@ -509,14 +509,51 @@ void show_value(const int &v){
     cout<<v<<endl;
 }
 
+string find_LCS(string str1,string str2){
+    int len1=str1.size();
+    int len2=str2.size();
+
+    vector<vector<int>> dp(len1+1,vector<int>(len2+1,0));
+
+    for(int i=1;i<=len1;i++){
+        for(int j=1;j<=len2;j++){
+            if (str1[i-1]==str2[j-1]){
+                dp[i][j]=dp[i-1][j-1]+1;
+            }else{
+                dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+    }
+
+    string lcs;
+
+    int i=len1,j=len2;
+    while(i>0&&j>0){
+        if (str1[i-1]==str2[j-1]){
+            lcs.push_back(str1[i-1]);
+            --i;
+            --j;
+        }
+        else if (dp[i-1][j]>dp[i][j-1]){
+            --i;
+        }else{
+            --j;
+        }
+     
+
+    }
+    reverse(lcs.begin(),lcs.end());
+    return lcs;
+}
+
 int main(){
 
-    const int &ref=10;
-
-    cout<<ref<<endl;
-
-    int a=10;
-    show_value(a);
+    string str1="abac";
+    string str2="cab";
+    
+    string str3=find_LCS(str1,str2);
+    cout<<"longest substring:"<<str3<<endl;
+    
 
 	
 
