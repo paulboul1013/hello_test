61efd385f9b23edfc75e696c576f9e7faf4be3f1
paulboul1013 <paulboul1013@gmail.com>
2025-03-05 07:25:19 +0800

    leetcode動態分析ep1

--- test_leetcode.cpp
+++ test_leetcode.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <algorithm>
 using namespace std;
 
 
@@ -41,11 +42,122 @@ bool checkPowersOfThree_v3(int n) {
 
 }
 
+int climbing_stairs(int n){
+
+    if (n==1) return 1;
+    if (n==2) return 2;
+
+    return climbing_stairs(n-1)+climbing_stairs(n-2);
+}
+
+
+
+
+int climbing_stairs_v2(int n){
+
+    if (n==0) return 0;
+    int prev2=0;
+    int prev1=1;
+    int curr=1;
+
+    for(int i=2;i<=n;i++){
+        curr=prev1+prev2;
+
+        prev2=prev1;
+
+        prev1=curr;
+    }
+
+    return curr;
+
+}
+
+
+int tribonacci(int n){
+    if (n==0) return 0;
+    if (n==1) return 1;
+    if (n==2) return 1;
+    int prev3=0;
+    int prev2=1;
+    int prev1=1;
+    int curr=prev1;
+
+    for(int i=3;i<=n;i++){
+        curr=prev1+prev2+prev1;
+
+        prev3=prev2;
+        prev2=prev1;
+        prev1=curr;
+    }
+
+    return curr;
+}
+
+int min(int a,int b){
+    if (a<b) return a;
+    return b;
+}
+
+int mincostclimbstairs(int *cost ,int costsize){
+    
+    int prev2=0;
+    int prev1=0;
+    int curr=prev1;
+
+    for(int i=2;i<=costsize;i++){
+        curr=min(prev1+cost[i-1],prev2+cost[i-2]);
+        prev2=prev1;
+        prev1=curr;
+    }
+
+    return curr;
+}
+
+int max(int a,int b){
+    if (a>b)return a;
+    return b;
+}
+
+int rob(int *nums,int numsize){
+    int prev2=0;
+    int prev1=nums[0];
+    int curr=prev1;
+    for(int i=2;i<=numsize;i++){
+        curr=max(nums[i-1]+prev2,prev1);
+
+        prev2=prev1;
+        prev1=curr;
+    }
+
+    return curr;
+}
+
+int cmp(const void*a,const void*b){
+    return *(const int*)a-*(const int*)b;
+}
+
+int deleteeandern_r(int *nums,int nums_size){
+    int k=nums_size-1;
+
+    while(nums[k-1]+1==nums[nums_size-1]){ 
+        k--;
+    }
+    return max(nums[nums_size-1]+deleteeandern_r(nums,k),deleteeandern_r(nums,nums_size-1));
+}
+
+int deleteandearn(int *num,int nums_size){
+    qsort(num,nums_size,sizeof(int),cmp);
+}
 
 
 int main() {
     // Write C++ code here
-    checkPowersOfThree_v3(12) ? cout<<"true":cout<<"false"<<endl;
+    // checkPowersOfThree_v3(12) ? cout<<"true":cout<<"false"<<endl;
+
+
+    cout<<tribonacci(40)<<endl;
+
 
     return 0;
+
 }
